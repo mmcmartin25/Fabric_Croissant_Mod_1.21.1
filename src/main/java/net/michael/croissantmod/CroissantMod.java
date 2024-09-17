d7b0979643c5c5e82b1d7034b58b82e6a8500db8
Michael McMartin <mmcmartin@Michaels-MacBook-Pro-2.local>
2024-09-17 15:58:12 -0400

    Initial commit

---
+++ src/main/java/net/michael/croissantmod/CroissantMod.java
@@ -0,0 +1,15 @@
+package net.michael.croissantmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CroissantMod implements ModInitializer {
+	public static final String MOD_ID = "croissantmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/michael/croissantmod/CroissantModClient.java
@@ -0,0 +1,10 @@
+package net.michael.croissantmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CroissantModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
