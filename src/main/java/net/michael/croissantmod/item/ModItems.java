139391a38f46a51c1867dc725e9fc8b46f111cf4
mmcmartin <michaelmcmartin@pm.me>
2024-09-18 20:51:30 -0400

    New items

--- src/main/java/net/michael/croissantmod/item/ModItems.java
+++ src/main/java/net/michael/croissantmod/item/ModItems.java
@@ -8,24 +8,24 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+
+
+
 public class ModItems {
 
     public static final Item CROISSANT = registerItem( "croissant", new Item(new Item.Settings()));
-    public static final Item CROISSANT_BLOCK = registerItem("croissant_block", new Item (new Item.Settings()));
-
+    public static final Item RAW_DOUGH = registerItem("raw_dough", new Item (new Item.Settings()));
+    public static final Item BUTTER = registerItem("butter", new Item (new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(CroissantMod.MOD_ID, name), item);
     }
-    public static void registerModItems() {
+        public static void registerModItems() {
         CroissantMod.LOGGER.info("Registering Mod Items for " + CroissantMod.MOD_ID);
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(CROISSANT_BLOCK);
-
-        });
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register((entries -> {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
             entries.add(CROISSANT);
-        }));
+            entries.add(RAW_DOUGH);
+            entries.add(BUTTER);
+        });
     }
 }
